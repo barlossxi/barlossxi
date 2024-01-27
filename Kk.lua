7865e61078696f69e59c57f134e65adfc0b61071
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-27 16:45:10 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -12,8 +12,6 @@ Tab:AddToggle({
 	Name = "Tw",
 	Default = false,
 	Callback = function(Value)
-_G.Tw = true
-while _G.Tw do wait()
 local TweenService = game:GetService("TweenService")
 local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
 {CFrame = CFrame.new(-387.568878, 2.65220118, 1648.24072, -0.0299262311, -4.77026383e-08, 0.999552131, -3.2846672e-09, 1, 4.76256687e-08, -0.999552131, -1.85793925e-09, -0.0299262311)}):Play()

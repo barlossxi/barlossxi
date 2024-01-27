bdc6a58be17017bc17d359780addcaa950d77639
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-27 16:14:44 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -1,5 +1,7 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
 local Window = OrionLib:MakeWindow({Name = "Barloss", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
 local Section = Tab:AddSection({
 	Name = "AutoFram"
 })
@@ -7,8 +9,9 @@ Tab:AddToggle({
 	Name = "AutoFram",
 	Default = false,
 	Callback = function(Value)
-		local TweenService = game:GetService("TweenService")
+		
+			local TweenService = game:GetService("TweenService")
 local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
 {CFrame = CFrame.new(-387.568878, 2.65220118, 1648.24072, -0.0299262311, -4.77026383e-08, 0.999552131, -3.2846672e-09, 1, 4.76256687e-08, -0.999552131, -1.85793925e-09, -0.0299262311)}):Play()
-	end    
+  end    
 })

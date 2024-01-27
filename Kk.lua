2d455b9e7bfb183931c74ada9b812610d7687dae
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-27 19:10:23 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Winnable Hub", "DarkTheme")
+local Window = Library.CreateLib("Barloss Hub", "DarkTheme")
 local Tab = Window:NewTab("Auto Farm")
 local Section = Tab:NewSection("Felix | ComeBxck")
  

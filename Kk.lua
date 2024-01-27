fddb561714b9b446b06e0f125d3d3d2cf1fe7082
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-27 19:35:14 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -1,18 +1,26 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Window = OrionLib:MakeWindow({Name = "Barloss", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 local Tab = Window:MakeTab({
 	Name = "Tab 1",
 	Icon = "rbxassetid://4483345998",
 	PremiumOnly = false
 })
 local Section = Tab:AddSection({
-	Name = "Section"
+	Name = "ออโต้ฟาม"
 })
 Tab:AddToggle({
-	Name = "This is a toggle!",
+	Name = "ออโต้ฟาม",
 	Default = false,
 	Callback = function(Value)
-		_G.AutoFarm = state
+
+MONS = {}
+ 
+for i,v in pairs(game:GetService("Workspace").Monster.Mon:GetChildren()) do
+    table.insert(MONS,v.Name)
+end
+ 
+Section:NewToggle("Auto-Farm", "", function(state)
+    _G.AutoFarm = state
     while _G.AutoFarm do wait()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Monster.Mon[Select].HumanoidRootPart.CFrame * CFrame.new(0,0,5)
 end

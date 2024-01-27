ef95ad6b3d4b1bf7f5e91b2485c7811b96270233
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-27 16:24:21 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -12,14 +12,16 @@ Tab:AddButton({
 	Name = "AutoFram!",
 	Callback = function()
       		print("button pressed")
+		local TweenService = game:GetService("TweenService")
+local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
+{CFrame = CFrame.new(-387.568878, 2.65220118, 1648.24072, -0.0299262311, -4.77026383e-08, 0.999552131, -3.2846672e-09, 1, 4.76256687e-08, -0.999552131, -1.85793925e-09, -0.0299262311)}):Play()
+			
   	end    
 })
 Tab:AddColorpicker({
 	Name = "AutoFram",
 	Default = Color3.fromRGB(255, 0, 0),
 	Callback = function(Value)
-		local TweenService = game:GetService("TweenService")
-local Tw = TweenService:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(1, Enum.EasingStyle.Linear, Enum.EasingDirection.Out,0,false,0), 
-{CFrame = CFrame.new(-387.568878, 2.65220118, 1648.24072, -0.0299262311, -4.77026383e-08, 0.999552131, -3.2846672e-09, 1, 4.76256687e-08, -0.999552131, -1.85793925e-09, -0.0299262311)}):Play()
+	
 	end	  
 })

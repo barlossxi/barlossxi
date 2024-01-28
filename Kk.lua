d864697cda7b112400c7aaa709c0f54baa2a4f08
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-29 02:28:57 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -1,123 +1,33 @@
-_G.Auto_Farm = true -- true / false
-
-function totarget(p)
-    local Distance2 = (p.Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-    local tween_s = game:service"TweenService"
-    local info = TweenInfo.new(Distance2/350, Enum.EasingStyle.Linear)
-    local tween = tween_s:Create(game:GetService("Players").LocalPlayer.Character["HumanoidRootPart"], info, {CFrame = p})
-    tween:Play()
-    if Distance2 <= 75 then
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = p
-    end
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = " Barloss", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Tab = Window:MakeTab({
+	Name = "Auto Fram",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Tab = Window:NewTab("Auto Farm")
+local Section = Tab:NewSection("Felix | ComeBxck")
+ 
+MONS = {}
+ 
+for i,v in pairs(game:GetService("Workspace").Monster.Mon:GetChildren()) do
+    table.insert(MONS,v.Name)
 end
-
-function checklevel()
-    local Level = game:GetService("Players").LocalPlayer.Data.Level.Value
-    if Level == 1 or Level <= 9 then
-        Mon = "Bandit [Level. 5]"
-        Title = "Bandit"
-        QuestName = "BanditQuest1"
-        QuestNumber = 1
-        CFrameQuest = CFrame.new(1059.7457275390625, 16.393512725830078, 1548.560546875)
-        CFrameMon = CFrame.new(976.605712890625, 16.34925651550293, 1555.198974609375)
-        CFramePuk = CFrame.new(1010.50732421875, 50.843017578125, 1633.6839599609375)
-    elseif Level == 10 or Level <= 14 then
-        Mon = "Monkey [Level. 14]"
-        Title = "Monkey"
-        QuestName = "JungleQuest"
-        QuestNumber = 1
-        CFrameQuest = CFrame.new(-1599.23096, 37.8653831, 153.335953, -0.0493941903, 1.29583286e-08, 0.998779356, 3.21716165e-08, 1, -1.13831318e-08, -0.998779356, 3.15700852e-08, -0.0493941903)
-        CFrameMon = CFrame.new(-1479.76099, 23.195364, 106.327942, 0.96289885, 5.22265786e-10, -0.269862473, 6.59528099e-10, 1, 4.28857172e-09, 0.269862473, -4.30744285e-09, 0.96289885)
-        CFramePuk = CFrame.new(-1776.32959, 61.1782455, 66.8902054, -0.912609756, -2.38546143e-08, 0.408831745, -2.14773621e-08, 1, 1.04056577e-08, -0.408831745, 7.15677129e-10, -0.912609756)
-        elseif lv == 15 or lv <= 29 then
-        Mon = "Gorilla [Lv. 20]"
-        Title = "Gorilla"
-        QuestName = "JungleQuest"
-        QuestNumber = 2
-        CFrameQuest = CFrame.new(-1599.23096, 37.8653831, 153.335953, -0.0493941903, 1.29583286e-08, 0.998779356, 3.21716165e-08, 1, -1.13831318e-08, -0.998779356, 3.15700852e-08, -0.0493941903)
-        CFrameMon = CFrame.new(-1242.46655, 6.62262297, -523.166382, -0.974416733, 9.23166681e-08, -0.224748924, 9.58993027e-08, 1, -5.02435071e-09, 0.224748924, -2.64490758e-08, -0.974416733)
-        CFramePuk = CFrame.new(-1133.4574, 40.8067436, -526.086792, 0.647179008, -2.76535794e-10, 0.762338042, 3.26674865e-08, 1, -2.73699801e-08, -0.762338042, 4.26169464e-08, 0.647179008)
-    end
+ 
+Section:NewToggle("Auto-Farm", "", function(state)
+    _G.AutoFarm = state
+    while _G.AutoFarm do wait()
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Monster.Mon[Select].HumanoidRootPart.CFrame * CFrame.new(0,0,5)
 end
-
-spawn(function()
-    while task.wait(.1) do
-        pcall(function()
-            if _G.Auto_Farm then
-            checklevel()
-                if game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
-totarget(CFrameQuest)
-wait(.3)
-local args = {
-    [1] = "StartQuest",
-    [2] = QuestName,
-    [3] = QuestNumber
-}
-
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-elseif game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == true then
-    for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-        for i,v2 in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-        if v.Name == Mon and v2.Name == Mon then
-            totarget(v.HumanoidRootPart.CFrame * CFrame.new(0,1,15))
-            v.HumanoidRootPart.Size = Vector3.new(60,2.5,60)
-            v.HumanoidRootPart.CFrame = CFrameMon
-            game:GetService'VirtualUser':CaptureController()
-            game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
-            v2.HumanoidRootPart.CanCollide = true
-            sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
-        end
-        end
-    end
-                end
-            end
-        end)
-    end
 end)
-
-spawn(function()
-    while task.wait(.1) do
-        pcall(function()
-            if _G.Auto_Farm then
-            checklevel()
-    if not string.find(game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Container.QuestTitle.Title.Text, Title) then
-local args = {
-    [1] = "AbandonQuest"
-}
-
-game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
-    end
-            end
-        end)
-    end
+ 
+Section:NewDropdown("Please Select Monster", "", MONS, function(currentOption)
+    Select = currentOption
 end)
-
-spawn(function()
-    while task.wait(.1) do
-        pcall(function()
-            if _G.Auto_Farm then
-            checklevel()
-            if not game:GetService("Workspace").Enemies:FindFirstChild(Mon) then
-                totarget(CFramePuk)
-            end
-            end
-        end)
-    end
-end)
-
-spawn(function()
-    while task.wait(.1) do
-        pcall(function()
-            if _G.Auto_Farm then
-            checklevel()
-            for i,v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
-        if v.Name == Mon then
-            if v.Humanoid.Health == 0 then
-            v:Destroy()
-            end
-            end
-            end
-            end
-        end)
-    end
+ 
+Section:NewButton("Refresh", "", function()
+    table.clear(MONS)
+for i,v in pairs(game:GetService("Workspace").Monster.Mon:GetChildren()) do
+    table.insert(MONS,v.Name)
+end
 end)

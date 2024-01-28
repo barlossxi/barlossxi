fe040469b0d9846fbc195adcfa1566c840644069
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-29 01:37:54 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -16,7 +16,7 @@ function checklevel()
     if lv == 1 or lv <= 9 then
         Mon = "Bandit [Lv. 5]"
         Title = "Bandit"
-        QuestName = "BanditQuest1"
+        QuestName = "BanditQuest1",1"
         QuestNumber = 1
         CFrameQuest = CFrame.new(1061.15271, 16.7367725, 1548.93018, -0.836085379, -3.89774577e-08, 0.548599303, -1.17575967e-08, 1, 5.31300408e-08, -0.548599303, 3.79710414e-08, -0.836085379)
         CFrameMon = CFrame.new(1151.11829, 16.7761021, 1599.73499, -0.999999762, 0, -0.000701809535, 0, 1, 0, 0.000701809535, 0, -0.999999762)

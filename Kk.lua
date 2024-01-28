5675b91218ceef8216a56c55bf4cd15bd581df7e
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-29 02:40:17 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -1,33 +1,24 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = " Barloss", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
-local Tab = Window:MakeTab({
-	Name = "Auto Fram",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-local Tab = Window:NewTab("Auto Farm")
-local Section = Tab:NewSection("Felix | ComeBxck")
+_G.AutoFarmLV = true ----true/false
  
-MONS = {}
+MON = "Bandit [Lv. 5]" --MonName
  
-for i,v in pairs(game:GetService("Workspace").Monster.Mon:GetChildren()) do
-    table.insert(MONS,v.Name)
-end
+    spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.AutoFarmLV then
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Enemies[MON].HumanoidRootPart.CFrame * CFrame.new(0,0,5) --Distace
+            end
+        end)
+       end)
+    end)
  
-Section:NewToggle("Auto-Farm", "", function(state)
-    _G.AutoFarm = state
-    while _G.AutoFarm do wait()
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Monster.Mon[Select].HumanoidRootPart.CFrame * CFrame.new(0,0,5)
-end
-end)
- 
-Section:NewDropdown("Please Select Monster", "", MONS, function(currentOption)
-    Select = currentOption
-end)
- 
-Section:NewButton("Refresh", "", function()
-    table.clear(MONS)
-for i,v in pairs(game:GetService("Workspace").Monster.Mon:GetChildren()) do
-    table.insert(MONS,v.Name)
-end
-end)
+    spawn(function()
+       game:GetService("RunService").RenderStepped:Connect(function()
+        pcall(function()
+            if _G.AutoFarmLV then
+            game:GetService'VirtualUser':CaptureController()
+            game:GetService'VirtualUser':Button1Down(Vector2.new(1280, 672))
+            end
+        end)
+       end)
+    end)

706915cdb935aad6b21c6184921d026c7ac21013
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-01-28 19:01:45 +0700

    Update Kk.lua

--- Kk.lua
+++ Kk.lua
@@ -13,6 +13,6 @@ Tab:AddToggle({
 	Default = false,
 	Callback = function(Value)
 		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-504.398895, 7.85236597, 1445.93567, -0.876919866, 0, -0.480636656, 0, 1, 0, 0.480636656, 0, -0.876919866)
-		end
+		
 	end    
 })

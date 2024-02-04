3dfdf7cf9aad4814d9a6c13290522e34af26ea6c
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-05 06:00:24 +0700

    Update ZOLA HUB.lua

--- ZOLA HUB.lua
+++ ZOLA HUB.lua
@@ -2,8 +2,8 @@ local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 local Window = Fluent:CreateWindow({
-    Title = "Zola Hub | Blox fruit",
-    SubTitle = "ดีครับพวไก่",
+    Title = "ZOLA Hub | Blox fruit",
+    SubTitle = "สวัดดีจ้าาา",
     TabWidth = 160,
     Size = UDim2.fromOffset(450, 300),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
@@ -11,16 +11,16 @@ local Window = Fluent:CreateWindow({
     MinimizeKey = Enum.KeyCode.End -- Used when theres no MinimizeKeybind
 })
 local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "home" }),
-    Setting = Window:AddTab({ Title = "Setting", Icon = "settings" }),
-    Stats = Window:AddTab({ Title = "Stats", Icon = "plus-circle" }),
-    Player = Window:AddTab({ Title = "Player", Icon = "baby" }),
-    Teleport = Window:AddTab({ Title = "Teleport", Icon = "palmtree" }),
-    Fruit = Window:AddTab({ Title = "Devil Fruit", Icon = "cherry" }),
-    Raid = Window:AddTab({ Title = "Dungeon", Icon = "swords" }),
-    Race = Window:AddTab({ Title = "Race V4", Icon = "chevrons-right" }),
-    Shop = Window:AddTab({ Title = "Shop", Icon = "shopping-cart" }),
-	Misc = Window:AddTab({ Title = "Misc", Icon = "list-plus" }),
+    Main = Window:AddTab({ Title = "เมนู", Icon = "home" }),
+    Setting = Window:AddTab({ Title = "การตั้งค่า", Icon = "settings" }),
+    Stats = Window:AddTab({ Title = "สเเต็ก", Icon = "plus-circle" }),
+    Player = Window:AddTab({ Title = "ผู้เล่น", Icon = "baby" }),
+    Teleport = Window:AddTab({ Title = "เทเลพอร์ต", Icon = "palmtree" }),
+    Fruit = Window:AddTab({ Title = "ผลปีศาจ", Icon = "cherry" }),
+    Raid = Window:AddTab({ Title = "ลงดัน", Icon = "swords" }),
+    Race = Window:AddTab({ Title = "เผ่าวี4", Icon = "chevrons-right" }),
+    Shop = Window:AddTab({ Title = "ร้านค้า", Icon = "shopping-cart" }),
+	Misc = Window:AddTab({ Title = "อื่น", Icon = "list-plus" }),
 }
 local Options = Fluent.Options
 

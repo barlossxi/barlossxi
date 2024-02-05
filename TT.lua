d8dbaf8e5e8b64ac6fa48705984a3b8809f06323
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-05 15:50:34 +0700

    Add files via upload

---
+++ TT.lua
@@ -0,0 +1,120 @@
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "ZOLA Hub | Blox fruit",
+    SubTitle = "By.Barloss",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(450, 300),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Aqua",
+    MinimizeKey = Enum.KeyCode.End -- Used when theres no MinimizeKeybind
+})
+local Tabs = {
+    Main = Window:AddTab({ Title = "เมนู", Icon = "home" }),
+    Setting = Window:AddTab({ Title = "ตั้งค่า", Icon = "settings" }),
+    Stats = Window:AddTab({ Title = "อัพค่าพลัง", Icon = "plus-circle" }),
+    Player = Window:AddTab({ Title = "ผู้เล่น", Icon = "baby" }),
+    Teleport = Window:AddTab({ Title = "เทเลพอร์ต", Icon = "palmtree" }),
+    Fruit = Window:AddTab({ Title = "ผลปีศาจ", Icon = "cherry" }),
+    Raid = Window:AddTab({ Title = "ลงดัน", Icon = "swords" }),
+    Race = Window:AddTab({ Title = "เผ่าวี4", Icon = "chevrons-right" }),
+    Shop = Window:AddTab({ Title = "ร้านค้า", Icon = "shopping-cart" }),
+	Misc = Window:AddTab({ Title = "อื่น", Icon = "list-plus" }),
+}
+local Options = Fluent.Options
+
+do
+----------------------------------------------------------------------
+
+LocalPlayer = game:GetService("Players").LocalPlayer
+Char = LocalPlayer.Character
+
+_G.AutoFarm = true
+local GetQuests = function(N,NB)
+    local args = {
+        [1] = "StartQuest",
+        [2] = N or "BanditQuest1",
+        [3] = NB or 1
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(args))    
+end;local ChackQ = function()
+    local Lv = LocalPlayer.Data.Level
+    if Lv.Value >= 1 and Lv.Value <= 9 then
+        return {
+            ["Mon"] = 'Bandit',
+            ["NumQ"] = 'BanditQuest1',
+            ["NameQ"] = 1,
+            ["CFrameQ"] = CFrame.new(1059.37195, 15.4495068, 1550.4231, 0.939700544, -0, -0.341998369, 0, 1, -0, 0.341998369, 0, 0.939700544),
+            ["CFrameMon"] = CFrame.new(1196.172, 11.8689699, 1616.95923, -0.309060812, 0, 0.951042235, 0, 1, 0, -0.951042235, 0, -0.309060812)
+        }
+    elseif Lv.Value >= 10 and Lv.Value <= 9e99 then
+        return {
+            ["Mon"] = 'Monkey',
+            ["NumQ"] = 'JungleQuest',
+            ["NameQ"] = 1,
+            ["CFrameQ"] = CFrame.new(-1598.08911, 35.5501175, 153.377838, 0, 0, 1, 0, 1, -0, -1, 0, 0),
+            ["CFrameMon"] = CFrame.new(-1619.10632, 21.7005882, 142.148117, 0.342042625, -0.000311157171, 0.939684391, 0.000113111477, 0.99999994, 0.000289957155, -0.939684391, 7.11137545e-06, 0.342042685)
+        }
+    end
+end;local TW = function(...)
+    local CFrame = {...}
+    pcall(function()
+        if not _G.StopTween then
+            local Distance = (CFrame[1].Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
+            Tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/20, Enum.EasingStyle.Cubic),{CFrame = CFrame[1]})
+            if _G.StopTween then Tween:Cancel()
+            elseif game.Players.LocalPlayer.Character.Humanoid.Health > 0 then Tween:Play() end
+            if not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("OMG Hub") then
+                local Noclip = Instance.new("BodyVelocity")
+                Noclip.Name = "OMG Hub"
+                Noclip.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
+                Noclip.MaxForce = Vector3.new(9e99,9e99,9e99)
+                Noclip.Velocity = Vector3.new(0,0,0)
+            end
+        end
+    end)
+end;local ClearQ = function()
+    if not string.find(game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Container.QuestTitle.Title.Text, tostring(ChackQ()["Mon"])) then
+        game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("AbandonQuest")
+    end
+end
+
+local ToggleAutoFarmLevel = Tabs.Main:AddToggle("ToggleAutoFarmLevel", {Title = "ออโต้ฟามเลเวล", Default = false })
+    ToggleAutoFarmLevel:OnChanged(function(Value)
+        _G.AutoLevel = Value
+    end)
+    Options.ToggleAutoFarmLevel:SetValue(false)
+spawn(function()
+    while wait() do
+        pcall(function()
+            if _G.AutoFarm then
+                local UIQ = LocalPlayer.PlayerGui.Main.Quest
+                ClearQ()
+                if not UIQ.Visible or UIQ.Visible == false then
+                    TW(ChackQ()["CFrameQ"])
+                    if (ChackQ()["CFrameQ"].Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 15 then
+                        wait(.2)
+                        GetQuests(ChackQ()["NumQ"],ChackQ()["NameQ"])
+                    end
+                else
+                    if game:GetService("Workspace").Enemies:FindFirstChild(ChackQ()["Mon"]) then
+                        for _i,_v in pairs(game:GetService("Workspace").Enemies:GetChildren()) do
+                            if _v.Name == tostring(ChackQ()["Mon"]) and _v:FindFirstChild("Humanoid") and _v:FindFirstChild("HumanoidRootPart") then
+                                if _v.Humanoid.Health > 0 then
+                                    repeat wait()
+                                        TW(_v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,5))
+                                        game:GetService("VirtualUser"):CaptureController()
+                                        game:GetService("VirtualUser"):Button1Down(Vector2.new(1280,672))
+                                    until not _G.AutoFarm or _G.AutoFarm == false or not _v.Parent or _v.Humanoid.Health <= 0 or not UIQ.Visible or UIQ.Visible == false
+                                end
+                            end
+                        end
+                    else
+                        Char.HumanoidRootPart.CFrame = ChackQ()["CFrameMon"]
+                    end
+                end
+            end
+        end)
+    end
+end)

10e6a0a679f8a4cdb88047d78b07010cfbfc1980
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-06 11:19:33 +0700

    Update TT.lua

--- TT.lua
+++ TT.lua
@@ -60,7 +60,7 @@ end;local TW = function(...)
     pcall(function()
         if not _G.StopTween then
             local Distance = (CFrame[1].Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude
-            Tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/50, Enum.EasingStyle.Cubic),{CFrame = CFrame[1]})
+            Tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart,TweenInfo.new(Distance/20, Enum.EasingStyle.Cubic),{CFrame = CFrame[1]})
             if _G.StopTween then Tween:Cancel()
             elseif game.Players.LocalPlayer.Character.Humanoid.Health > 0 then Tween:Play() end
             if not game.Players.LocalPlayer.Character.HumanoidRootPart:FindFirstChild("OMG Hub") then
@@ -68,7 +68,7 @@ end;local TW = function(...)
                 Noclip.Name = "OMG Hub"
                 Noclip.Parent = game.Players.LocalPlayer.Character.HumanoidRootPart
                 Noclip.MaxForce = Vector3.new(9e99,9e99,9e99)
-                Noclip.Velocity = Vector3.new(0,10,0)
+                Noclip.Velocity = Vector3.new(0,0,0)
             end
         end
     end)
@@ -92,7 +92,7 @@ spawn(function()
                 ClearQ()
                 if not UIQ.Visible or UIQ.Visible == false then
                     TW(ChackQ()["CFrameQ"])
-                    if (ChackQ()["CFrameQ"].Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 15 then
+                    if (ChackQ()["CFrameQ"].Position - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude <= 50 then
                         wait(.2)
                         GetQuests(ChackQ()["NumQ"],ChackQ()["NameQ"])
                     end
@@ -102,7 +102,7 @@ spawn(function()
                             if _v.Name == tostring(ChackQ()["Mon"]) and _v:FindFirstChild("Humanoid") and _v:FindFirstChild("HumanoidRootPart") then
                                 if _v.Humanoid.Health > 0 then
                                     repeat wait()
-                                        TW(_v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,0,5))
+                                        TW(_v:FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,10,0))
                                         game:GetService("VirtualUser"):CaptureController()
                                         game:GetService("VirtualUser"):Button1Down(Vector2.new(1280,672))
                                     until not _G.AutoFarm or _G.AutoFarm == false or not _v.Parent or _v.Humanoid.Health <= 0 or not UIQ.Visible or UIQ.Visible == false

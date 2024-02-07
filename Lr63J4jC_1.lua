1d14a7ebf7cfa70c7bd87cc3ae892e09359c089b
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-08 02:19:30 +0700

    Update Lr63J4jC_1.lua

--- Lr63J4jC_1.lua
+++ Lr63J4jC_1.lua
@@ -1,20 +1,48 @@
-local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/AZYsGithub/DrRay-UI-Library/main/DrRay.lua"))()
-local window = DrRayLibrary:Load("KIMUJI HUB", "rbxassetid://7743874674")
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
- 
-wait()
-local Sound = Instance.new("Sound",game:GetService("SoundService"))
-Sound.SoundId = "rbxassetid://5656490592"
-Sound.Volume = 10
-Sound:Play()
- 
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "ZOLA Hub | Blox fruit",
+    SubTitle = "By.Barloss",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(450, 300),
+    Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
+    Theme = "Aqua",
+    MinimizeKey = Enum.KeyCode.End -- Used when theres no MinimizeKeybind
+})
+local Tabs = {
+    Main = Window:AddTab({ Title = "เมนู", Icon = "home" }),
+    Setting = Window:AddTab({ Title = "ตั้งค่า", Icon = "settings" }),
+    Stats = Window:AddTab({ Title = "อัพค่าพลัง", Icon = "plus-circle" }),
+    Player = Window:AddTab({ Title = "ผู้เล่น", Icon = "baby" }),
+    Teleport = Window:AddTab({ Title = "เทเลพอร์ต", Icon = "palmtree" }),
+    Fruit = Window:AddTab({ Title = "ผลปีศาจ", Icon = "cherry" }),
+    Raid = Window:AddTab({ Title = "ลงดัน", Icon = "swords" }),
+    Race = Window:AddTab({ Title = "เผ่าวี4", Icon = "chevrons-right" }),
+    Shop = Window:AddTab({ Title = "ร้านค้า", Icon = "shopping-cart" }),
+	Misc = Window:AddTab({ Title = "อื่น", Icon = "list-plus" }),
+}
+local Options = Fluent.Options
+--------------------------------------------------------------------------------------------------------------------------------------------
+
+local ToggleBone = Tabs.Main:AddToggle("ToggleBone", {Title = "ออโต้ตีบอบ", Default = false })
+ToggleBone:OnChanged(function(Value)
+    _G.AutoBone = Value
+end)
+spawn(function()
+        while task.wait() do
+        if _G.AutoBone then
+        pcall(function()
 Notification:Notify(
    {Title = "AutoClicker Controls?", Description = "'V' To Toggle AutoClick Escape! 'B' To AutoLock Mouse Position"},
    {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 35, Type = "option"},
    {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
  
 )
+spawn(function()
+        while task.wait() do
+        if _G.AutoBone then
+        pcall(function()
 getgenv().Settings = {
     ["Auto Click Keybind"] = "V", -- Use an UpperCase letter or KeyCode Enum. Ex: Enum.KeyCode.Semicolon
     ["Lock Mouse Position Keybind"] = "B",
@@ -22,11 +50,3 @@ getgenv().Settings = {
     ["GUI"] = true, -- A drawing gui that tells you what is going on with the autoclicker.
     ["Delay"] = 0 -- 0 for RenderStepped, other numbers go to regular wait timings.
 }
-loadstring(game:HttpGet("https://pastebin.com/raw/5CHXWjDD"))()
---------------------
---Autoclicker
---------------------
- 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ForYourHead/AutoParry/main/AutoParryNoGui"))()
-loadstring(game:HttpGet(('https://pastebin.com/raw/JXdEbN7u'),true))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/Circle"))()

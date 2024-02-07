a66a54fabea64b1dad7c28fc0fdbdd42362f8004
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-08 02:28:58 +0700

    Update Lr63J4jC_1.lua

--- Lr63J4jC_1.lua
+++ Lr63J4jC_1.lua
@@ -31,17 +31,7 @@ ToggleBone:OnChanged(function(Value)
 end)
 spawn(function()
         while task.wait() do
-        if _G.AutoBone then
-        pcall(function()
-Notification:Notify(
-   {Title = "AutoClicker Controls?", Description = "'V' To Toggle AutoClick Escape! 'B' To AutoLock Mouse Position"},
-   {OutlineColor = Color3.fromRGB(80, 80, 80),Time = 35, Type = "option"},
-   {Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = function(State) print(tostring(State)) end}
- 
-)
-spawn(function()
-        while task.wait() do
-        if _G.AutoBone then
+        if Auto Click Keybind then
         pcall(function()
 getgenv().Settings = {
     ["Auto Click Keybind"] = "V", -- Use an UpperCase letter or KeyCode Enum. Ex: Enum.KeyCode.Semicolon
@@ -50,3 +40,5 @@ getgenv().Settings = {
     ["GUI"] = true, -- A drawing gui that tells you what is going on with the autoclicker.
     ["Delay"] = 0 -- 0 for RenderStepped, other numbers go to regular wait timings.
 }
+end
+end

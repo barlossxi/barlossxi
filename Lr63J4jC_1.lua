fd9b32ffd3905c1ab29eb5d5d88353fb84cf7b85
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-08 22:01:19 +0700

    Update Lr63J4jC_1.lua

--- Lr63J4jC_1.lua
+++ Lr63J4jC_1.lua
@@ -2423,9 +2423,9 @@ local posZ = 0
           CheckLevel()
           if not string.find(game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Container.QuestTitle.Title.Text, NameMon) or game:GetService("Players").LocalPlayer.PlayerGui.Main.Quest.Visible == false then
           game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer("AbandonQuest")
-          if BypassTP false
-          if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - CFrameQ.Position).Magnitude > 5000 then
-          BTP(CFrameQ)
+          if Tween then
+          if (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - CFrameQ.Position).Magnitude > 1 then
+          Tween(CFrameQ)
           elseif (game.Players.LocalPlayer.Character.HumanoidRootPart.Position - CFrameQ.Position).Magnitude < 1 then
           Tween(CFrameQ)
           end

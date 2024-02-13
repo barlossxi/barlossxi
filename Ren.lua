10787ec10544dd8538201842750c6dd890661eb0
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-14 00:46:27 +0700

    Update Ren.lua

--- Ren.lua
+++ Ren.lua
@@ -1,15 +1,1 @@
-_G.Color = Color3.fromRGB(255, 0, 0)
-_G.Logo = 12781257228
-
-
-local Evil = loadstring(game:HttpGet("https://raw.githubusercontent.com/lntanon55/Golf-HUB/main/Protected_9405524596134885.lua.txt"))()
-local Win = library:Evil("Name Hub","",_G.Logo )
-
-local tab1 = Win:CraftTab('Main')
-local page1 = tab1:CraftPage('Main',1)
-
-local player = page1:Label('Label')
-
-page1:Toggle('Test',nil,function(a)
-    print(a)
-end)
+ก

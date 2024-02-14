552ccddda955bdb19fd382ffd9ff90fd934fcb9f
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-14 19:05:55 +0700

    Update Ren.lua

--- Ren.lua
+++ Ren.lua
@@ -1,1 +1,11 @@
-บ
+local CombatFramework = require(game:GetService("Players").LocalPlayer.PlayerScripts.CombatFramework)
+local Camera = require(game.ReplicatedStorage.Util.CameraShaker)
+Camera:Stop()
+coroutine.wrap(function()
+    game:GetService("RunService").Stepped:Connect(function()
+        if getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack then
+            getupvalues(CombatFramework)[2]['activeController'].timeToNextAttack = 0
+            getupvalues(CombatFramework)[2]['activeController'].hitboxMagnitude = 25
+        end
+    end)
+end)()

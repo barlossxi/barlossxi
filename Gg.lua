8dffb39f60df46a2ae148b78028c59d2b2284e5d
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-21 22:22:35 +0700

    Update Gg.lua

--- Gg.lua
+++ Gg.lua
@@ -139,11 +139,10 @@ for i ,v in pairs(game:GetService("Workspace").Shop:GetChildren()) do
 table.insert(shop,v.Name)
 end
 
-page4:Dropdown("SHOP",,"shop",function(a)
-    shop = a
+page1:Dropdown("Dropdown",{""},{"shop"},function(v)
+    shop = v
 end)
 
-
-page2:Button('TP SHOP',function()
+page3:Button('TP SHOP',function()
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.workspace.Shop[shop].CFrame * CFrame.new(0,5,0)
 end)

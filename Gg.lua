ada82b26b61ed3223bfce33e39e3d8285edcd758
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-21 16:39:23 +0700

    Update Gg.lua

--- Gg.lua
+++ Gg.lua
@@ -1,9 +1,9 @@
 _G.Color = Color3.fromRGB(255, 0, 0)
-_G.Logo = 1222
+_G.Logo = 12781257228
 
 
 local Evil = loadstring(game:HttpGet("https://raw.githubusercontent.com/lntanon55/Golf-HUB/main/Protected_9405524596134885.lua.txt"))()
-local Win = library:Evil("ZOLA Hub","",_G.Logo )
+local Win = library:Evil("Name Hub","",_G.Logo )
 
 local tab1 = Win:CraftTab('Main')
 local page1 = tab1:CraftPage('Main',1)
@@ -21,8 +21,7 @@ game.Players.LocalPlayer.Character.HumanoidRootPart .CFrame = v.Parent.CFrame
 wait()
             end
       end
-end
-	end    
+end  
 end)
 
 page1:Toggle('Auto Chests',false,function(d)
@@ -34,6 +33,5 @@ fireproximityprompt(v,30)
 game.Players.LocalPlayer.Character.HumanoidRootPart .CFrame = v.Parent.CFrame
 end
       end
-            end
-	end    
+            end   
 end)

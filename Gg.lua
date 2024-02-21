83e07648d595604d0f8342aa799e97752ed7648e
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-21 17:09:51 +0700

    Update Gg.lua

--- Gg.lua
+++ Gg.lua
@@ -1,3 +1,4 @@
+
 _G.Color = Color3.fromRGB(0, 247, 255)
 _G.Logo = 16341514086
 
@@ -7,8 +8,7 @@ local Win = library:Evil("ZOLA Hub","",_G.Logo )
 
 local tab1 = Win:CraftTab('Main')
 local page1 = tab1:CraftPage('Main',1)
-local tab2 = Win:CraftTab('Main')
-local page2 = tab1:CraftPage('Main',2)
+local page2 = tab2:CraftPage('Main',1)
 
 
 local player = page1:Label('TP')
@@ -37,3 +37,52 @@ end
       end
             end   
 end)
+
+
+page2:Button('Bandit Island',function()
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-898.974243, 34.5999947, -559.051453, 0.0182777327, 3.36339987e-08, -0.999832928, -6.71313103e-08, 1, 3.24124017e-08, 0.999832928, 6.65276758e-08, 0.0182777327)
+wait(0.1)
+for i,v in pairs(game:GetService("Workspace").NPC:GetDescendants()) do
+if v.ClassName == "ProximityPrompt" then
+fireproximityprompt(v,30)
+end
+end
+
+  	end)
+
+page2:Button('Clown Pirate Island',function()
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(74.4107437, 36.9179535, 79.3588791, -0.999664903, 3.31894299e-08, -0.0258848667, 3.20130162e-08, 1, 4.58622864e-08, 0.0258848667, 4.50182682e-08, -0.999664903)
+wait(0.1)
+for i,v in pairs(game:GetService("Workspace").NPC:GetDescendants()) do
+if v.ClassName == "ProximityPrompt" then
+fireproximityprompt(v,30)
+end
+end
+
+  	end)
+
+page2:Button('Marine Island',function()
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(886.932739, 35.5073013, 1153.22998, -0.0238624346, 4.11765164e-08, -0.999715269, 1.07852067e-07, 1, 3.86138979e-08, 0.999715269, -1.06899932e-07, -0.0238624346)
+wait(0.1)
+for i,v in pairs(game:GetService("Workspace").NPC:GetDescendants()) do
+if v.ClassName == "ProximityPrompt" then
+fireproximityprompt(v,30)
+end
+end
+
+  	end)
+
+page2:Button('Monkey Island Island',function()
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(735.348877, 42.3243103, -1181.01147, -0.0751134604, -5.73425822e-08, -0.997174978, -4.65505181e-08, 1, -5.39985585e-08, 0.997174978, 4.23629949e-08, -0.0751134604)
+wait(0.1)
+for i,v in pairs(game:GetService("Workspace").NPC:GetDescendants()) do
+if v.ClassName == "ProximityPrompt" then
+fireproximityprompt(v,30)
+end
+end
+
+  	end)

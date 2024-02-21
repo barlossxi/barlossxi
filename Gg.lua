151300a435c9391cb1e805ff39cbbf6dd55a44cd
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-21 16:55:10 +0700

    Update Gg.lua

--- Gg.lua
+++ Gg.lua
@@ -1,14 +1,17 @@
-_G.Color = Color3.fromRGB(255, 0, 0)
-_G.Logo = 12781257228
+_G.Color = Color3.fromRGB(0, 247, 255)
+_G.Logo = 16341514086
 
 
 local Evil = loadstring(game:HttpGet("https://raw.githubusercontent.com/lntanon55/Golf-HUB/main/Protected_9405524596134885.lua.txt"))()
-local Win = library:Evil("Name Hub","",_G.Logo )
+local Win = library:Evil("ZOLA Hub","",_G.Logo )
 
 local tab1 = Win:CraftTab('Main')
 local page1 = tab1:CraftPage('Main',1)
 local tab2 = Win:CraftTab('Main')
 local page2 = tab1:CraftPage('Main',2)
+
+
+local player = page1:Label('TP')
 
 
 page1:Toggle('Auto TP Item',false,function(K)

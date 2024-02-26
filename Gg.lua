cf206d4501b038bbb13d24e78ce242a6f5632eaf
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-26 23:33:08 +0700

    Update Gg.lua

--- Gg.lua
+++ Gg.lua
@@ -81,24 +81,24 @@ local player = page7:Label('Boss')
 
 
 
-page7:Dropdown("Select Mobs", {"Sukuna"}, 1, function(text)
-if text == "Sukuna" then
+page1:Dropdown("Select Mobs", {"Bandit"}, 1, function(text)
+if text == "Bandit" then
     function mobs()
     spawn(function()
         _G.mobs = true
         while _G.mobs do wait()
             pcall(function()
-                local function GetClosestPlayer()
-                local target = nil
+                local function Player()
+                local addcc = nil
                 for i,v in pairs(workspace.Lives:GetDescendants()) do
                     if v.Name == "Humanoid" and v.MaxHealth == 50  then
-                        target = v
+                        addcc = v
                     end
                  end
-                 return target
+                 return addcc
                 end
                  repeat task.wait()
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = GetClosestPlayer().RootPart.CFrame*CFrame.new(0,5,0)*CFrame.Angles(math.rad(-90),0,0)
+                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = Player().RootPart.CFrame*CFrame.new(0,5,0)*CFrame.Angles(math.rad(-90),0,0)
                 until _G.mobs == false
                 wait()
             end)
@@ -109,53 +109,7 @@ end
 end)
 
 
-page7:Toggle("Mobs Farm", function(value)
-    _G.mobs = value
-    print('mobs: ', value);
-    if value then
-        mobs();
-        _G.mobs = true
-        else
-        _G.mobs = false
-    end 
-end)
-
-
-
-______________________________________________________________________________        
-
-
-
-
-page7:Dropdown("Select Mobs", {"SukunaPY"}, 1, function(text)
-if text == "SukunaPY" then
-    function mobs()
-    spawn(function()
-        _G.mobs = true
-        while _G.mobs do wait()
-            pcall(function()
-                local function GetClosestPlayer()
-                local target = nil
-                for i,v in pairs(workspace.Lives:GetDescendants()) do
-                    if v.Name == "Humanoid" and v.MaxHealth == 12500  then
-                        target = v
-                    end
-                 end
-                 return target
-                end
-                 repeat task.wait()
-                    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = GetClosestPlayer().RootPart.CFrame*CFrame.new(0,5,0)*CFrame.Angles(math.rad(-90),0,0)
-                until _G.mobs == false
-                wait()
-            end)
-        end
-    end)
-end
-end
-end)
-
-
-page7:Toggle("Mobs Farm", function(value)
+page1:Toggle("Mobs Farm", function(value)
     _G.mobs = value
     print('mobs: ', value);
     if value then
@@ -165,16 +119,6 @@ page7:Toggle("Mobs Farm", function(value)
         _G.mobs = false
     end
 end)
-
-
-
-
-
-
-
-
-
-
 
 
 local player = page1:Label('TP')

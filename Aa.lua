149c81ba52f542538b7af405d3bc091dde6678ce
barlossxi <157821743+barlossxi@users.noreply.github.com>
2024-02-28 17:06:27 +0700

    Update Aa.lua

--- Aa.lua
+++ Aa.lua
@@ -50,7 +50,7 @@ end
 end)
 
     
-    local _G.SelectWeapon = {}
+    local Weaponlist = {}
 local Weapon = nil
 
 for i,v in pairs(game:GetService("Players").LocalPlayer.Backpack:GetChildren()) do
